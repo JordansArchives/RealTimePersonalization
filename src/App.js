f18912630d043a4900b588ea4420d2688ca94944
Jordan Watkins <watkinsconnor30@gmail.com>
2022-11-22 18:19:22 -0700

    add Chakra card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Center h="100vh">
+          <Box p="5" maxW="320px" borderWidth="1px">
+            <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+            <Flex align="baseline" mt={2}>
+              <Badge colorScheme="pink">Plus</Badge>
+              <Text
+                ml={2}
+                textTransform="uppercase"
+                fontSize="sm"
+                fontWeight="bold"
+                color="pink.800"
+              >
+                Verified &bull; Cape Town
+              </Text>
+            </Flex>
+            <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+              Modern, Chic Penthouse with Mountain, City & Sea Views
+            </Text>
+            <Text mt={2}>$119/night</Text>
+            <Flex mt={2} align="center">
+              <Text ml={1} fontSize="sm">
+                <b>4.84</b> (190)
+              </Text>
+            </Flex>
+          </Box>
+        </Center>
   );
 }
 
